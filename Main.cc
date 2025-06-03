3f7da2d80c0726d3e6e4fb84a1f46d4d8198f6ce
Pavel Korotkevich <pawday@mail.ru>
2025-06-03 22:07:03 +0300

    Fix -Wsign-compare

--- Main.cc
+++ Main.cc
@@ -13,9 +13,10 @@ int main(int argc, char **argv)
         std::cerr << std::format("WTF, argc({}) < 1", argc) << '\n';
         return EXIT_FAILURE;
     }
+    size_t uargc = argc;
 
     std::vector<std::string> argv_vec;
-    for (size_t arg_idx = 1; arg_idx != argc; ++arg_idx) {
+    for (size_t arg_idx = 1; arg_idx != uargc; ++arg_idx) {
         argv_vec.emplace_back(argv[arg_idx]);
     }
 
