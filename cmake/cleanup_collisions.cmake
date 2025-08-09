9b3200b60205db618955960686b882186e060bf6
Pavel Korotkevich <pawday@mail.ru>
2025-08-09 14:47:26 +0300

    cmake: object based buildint (regular + PIC), error on cmake function collision and cmake options cleanup

---
+++ cmake/cleanup_collisions.cmake
@@ -0,0 +1,7 @@
+function(target_strict_compilation TARGET)
+    message(FATAL_ERROR "Collision with target_strict_compilation from wl_gena")
+endfunction()
+
+function(target_cxx23 TARGET)
+    message(FATAL_ERROR "Collision with target_cxx23 from wl_gena")
+endfunction()
--- cmake/target_strict_compilation.cmake
+++ cmake/target_strict_compilation.cmake
@@ -1,14 +1,16 @@
-set(WSHADOW_WARN_OPT "-Wshadow")
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    set(WSHADOW_WARN_OPT "-Wshadow-all")
-endif()
+function(target_strict_compilation TARGET)
+    set(WSHADOW_WARN_OPT "-Wshadow")
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        set(WSHADOW_WARN_OPT "-Wshadow-all")
+    endif()
 
-target_compile_options(${TARGET_TO_STRICTIFY} PRIVATE
-    "-Wall"
-    "-Wextra"
-    "-pedantic-errors"
-    ${WSHADOW_WARN_OPT}
-    "-Werror"
-)
+    target_compile_options(${TARGET} PRIVATE
+        "-Wall"
+        "-Wextra"
+        "-pedantic-errors"
+        ${WSHADOW_WARN_OPT}
+        "-Werror"
+    )
 
-set_property(TARGET ${TARGET_TO_STRICTIFY} PROPERTY LINK_LIBRARIES_ONLY_TARGETS ON)
+    set_property(TARGET ${TARGET} PROPERTY LINK_LIBRARIES_ONLY_TARGETS ON)
+endfunction()
